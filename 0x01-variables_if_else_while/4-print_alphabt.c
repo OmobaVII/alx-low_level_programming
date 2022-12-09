ddf73e255428af31d4f7b77a4d0cf93bc89a60a6
Sanni <omoba.career@gmail.com>
2022-12-09 06:11:29 -0800

    Created 4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - lowercase alphabets without q and e
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		if ((letter != 'q') && (letter != 'e'))
+		{
+		putchar(letter);
+		}
+		letter++;
+	}
+	putchar('\n');
+	return (0);
+}
