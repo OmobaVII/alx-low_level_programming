c4516e5d98c5e52b3a1789a880abe57791665911
Sanni <omoba.career@gmail.com>
2022-12-13 03:03:16 -0800

    Created 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Program prints _putchar
+ * Return: Always 0 (Success)
+ */
+int main()
+{
+	char ch = '_';
+	char p = 'p';
+	char u = 'u';
+	char t = 't';
+	char c = 'c';
+	char h = 'h';
+	char a = 'a';
+	char r = 'r';
+
+	putchar(ch);
+	putchar(p);
+	putchar(u);
+	putchar(t);
+	putchar(c);
+	putchar(h);
+	putchar(a);
+	putchar(r);
+	putchar('\n');
+	return (0);
+	}
