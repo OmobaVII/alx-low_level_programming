ec7daa5df1d88809d7cff2dd53bea0f8f0261765
Sanni <omoba.career@gmail.com>
2022-12-19 14:10:56 -0800

    Created files for 2 adavanced tasks

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+int main(void)
+{
+	int random;
+	int counter;
+	int total;
+
+	srand(time(NULL));
+	for (counter = 0, total = 2772; total > 122; counter++)
+	{random = (rand() % 125) + 1;
+		printf("%c", random);
+		total -= random;
+	}
+	printf("%c", total);
+
+	return (0);
+}
