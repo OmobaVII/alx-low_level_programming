102c6663e0022ced0a04fdef972cbdf31ffcdcd1
Sanni <omoba.career@gmail.com>
2022-12-19 07:12:34 -0800

    Created 0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - used to reset the value of an int to 98
+ * @n: character used to test
+ * Return: 0 (Success)
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+
+}
