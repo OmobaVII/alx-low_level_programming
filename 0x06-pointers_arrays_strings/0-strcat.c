1b3b488f312a4c1912083928efcdd63cfe7de3fc
Sanni <omoba.career@gmail.com>
2022-12-21 00:41:27 -0800

    created 0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * *_strcat - for concetenating two strings
+ * @dest: pointer
+ * @src: second pointer
+ * Return: Always 0 (Success)
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int a, b;
+
+	for (a = 0; dest[a] != '\0'; a++);
+
+	for (b = 0; src[b] != '\0'; b++)
+	{
+		dest[a + b] = src[b];
+	}
+	return (dest);
+}
