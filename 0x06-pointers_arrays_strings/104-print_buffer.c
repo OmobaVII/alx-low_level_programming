e1f3b053c36149dc6f75d29993d34ca00c553240
Sanni <omoba.career@gmail.com>
2022-12-22 11:10:01 -0800

    Correction

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -2,50 +2,49 @@
 #include <stdio.h>
 
 /**
- * print_buffer - print a buffer only 10bytes at a time
- * @b: string to print buffer from
- * @size: size of buffer
- * Return: void
+ * print_buffer - Prints a buffer 10 bytes at a time, starting with
+ * the byte position, then showing the hex content,
+ * then displaying printable charcaters.
+ * @b: The buffer to be printed.
+ * @size: The number of bytes to be printed from the buffer.
  */
-
 void print_buffer(char *b, int size)
 {
-	int i, appex;
+	int byte, index;
 
-	for (i = 0; i < size; i += 10)
+	for (byte = 0; byte < size; byte += 10)
 	{
-		printf("%08x:", i);
+		printf("%08x: ", byte);
 
-		for (appex = 0; appex < 10; appex++)
+		for (index = 0; index < 10; index++)
 		{
-			if ((appex + i) >= size)
-				printf(" ");
-			else
-				printf("%02x", *(b + appex + i));
+			if ((index + byte) >= size)
+				printf("  ");
 
-			if ((appex % 2) != 0 && appex != 0)
+			else
+				printf("%02x", *(b + index + byte));
+
+			if ((index % 2) != 0 && index != 0)
 				printf(" ");
 		}
-
-		for (appex = 0; appex < 10; appex++)
+			for (index = 0; index < 10; index++)
 		{
-			if ((appex + i) >= size)
+			if ((index + byte) >= size)
 				break;
 
-			else if (*(b + appex + i) >= 31 &&
-				*(b + appex + i) <= 126)
-				printf("%c", *(b + appex + i));
+			else if (*(b + index + byte) >= 31 && *(b + index + byte) <= 126)
+				printf("%c", *(b + index + byte));
+
 			else
+
 				printf(".");
+
 		}
-		
-		if (i >= size)
-			continue;
+			if (byte >= size)
+				continue;
 
-
-		printf("\n");
+			printf("\n");
 	}
-
 	if (size <= 0)
 		printf("\n");
 }
