9688e8727471b3209b3613c9c130576b88108545
Sanni <omoba.career@gmail.com>
2023-01-06 09:50:24 -0800

    betty correction Task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,6 +5,7 @@
  * main - a function used to print the name of the program
  * @argc: number of arguments
  * @argv: the pointer to the argument
+ * Return: Success(0)
  */
 
 int main(int argc, char *argv[])
