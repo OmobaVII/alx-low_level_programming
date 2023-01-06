39f88860143efff4345b9c3b63ed0d79cd562a83
Sanni <omoba.career@gmail.com>
2023-01-06 14:12:21 -0800

    Task 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+/**
+ * is_num - used to identify is the pointer is all numbers
+ * @p: point to a value
+ * Return: Sucess (1)
+ */
+
+bool is_num(char *p)
+{
+	int b = 0;
+
+	for (b = 0; p[b]; b++)
+	{
+		if (!(p[b] >= '0' && p[b] <= '9'))
+			return (0);
+	}
+	return (1);
+}
+
+
+/**
+ * main - a function that multiplies 2 numbers
+ * @argc: number of arguments
+ * @argv: second test number
+ * Return: Success(0)
+ */
+
+int main(int argc, char *argv[])
+{
+	int a = 1;
+	int answer = 0;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (1);
+	}
+	while (a < argc)
+	{
+
+		if (is_num(argv[a]))
+			answer += atoi(argv[a]);
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+		a++;
+	}
+
+	printf("%d\n", answer);
+
+	return (0);
+}
