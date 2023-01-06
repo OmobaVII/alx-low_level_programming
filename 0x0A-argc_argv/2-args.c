18fe049b71edac30973ecf980e9abb284089c219
Sanni <omoba.career@gmail.com>
2023-01-06 09:23:01 -0800

    Task 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - function that print all arguments 1 per line.
+ * @argc: number of arguments
+ * @argv: pointer to the arguments
+ * Return: Success (0)
+ */
+
+int main(int argc, char *argv[])
+{
+	int a;
+	
+	for (a = 0; a < argc; a++)
+	printf("%s\n", argv[a]);
+	return (0);
+}
