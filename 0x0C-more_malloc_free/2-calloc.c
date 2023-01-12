f9119693d013c7f94221167e6d1812696b7c17d5
Sanni <omoba.career@gmail.com>
2023-01-12 05:35:20 -0800

    Correction Task 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -13,11 +13,11 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	char *p;
 	unsigned int a;
 
-	p = malloc(nmemb * size);
 	if (nmemb == 0 || size == 0)
 	{
 		return (NULL);
 	}
+	p = malloc(nmemb * size);
 	if (p == NULL)
 	{
 		return (NULL);
