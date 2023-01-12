dbbfb9ea84ef37b29ae2bc7b37a5e1376255e239
Sanni <omoba.career@gmail.com>
2023-01-12 03:45:14 -0800

    Task 1

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,16 +1,23 @@
 #include "main.h"
 #include <stdlib.h>
 
+/**
+ * malloc_checked - a function that assigns dynamic memory allocation
+ * @b: test value
+ * Return: Void
+ */
+
 void *malloc_checked(unsigned int b)
 {
 	void *p = malloc(sizeof(b));
+
 	if (b <= 0)
 	{
 		return (NULL);
 	}
 	if (p == NULL)
 	{
-		exit (98);
+		exit(98);
 	}
 
 	return (p);
---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,53 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * string_nconcat - a function that concatenates two strings
+ * @s1: The First string
+ * @s2: The second string
+ * @n: the number of characters to concatenates in the second string
+ * Return: the concatenated string
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *s;
+	unsigned int a, b, i;
+	unsigned int lenghts;
+
+	a = 0;
+	b = 0;
+
+	if (n < strlen(s2))
+	{
+		lenghts = (strlen(s1) + strlen(s2) - n + 1);
+	}
+	else
+	{
+		(lenghts = strlen(s1) + strlen(s2) + 1);
+	}
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+	s = malloc(lenghts + 1);
+	if (s == NULL)
+		return (NULL);
+	while (a < strlen(s1))
+	{
+		s[a] = s1[a];
+		a++;
+	}
+	for (i = a; i < (lenghts + 1); i++)
+	{
+		s[i] = s2[b];
+		b++;
+	}
+	return (s);
+}
