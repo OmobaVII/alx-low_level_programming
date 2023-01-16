cdeb830d932de184224499388e5d25195a1c35b1
Sanni <omoba.career@gmail.com>
2023-01-16 07:36:32 -0800

    Task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef __DOG_H__
+#define __DOG_H__
+
+/**
+ * struct dog - to give the name age and owner of a dog
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the name of the owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
