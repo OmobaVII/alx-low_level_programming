15b261681af264954f154ac828bdd8df0b62c21e
Sanni <omoba.career@gmail.com>
2023-01-18 12:09:56 -0800

    Correction

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,21 +12,14 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int a = 0;
 
-	if (size <= 0 && cmp == NULL)
+	if (array == NULL || size <= 0 || cmp == NULL)
 		return (-1);
-	if (array != NULL)
+	for (a = 0; a < size; a++)
 	{
-		for (a = 0; a < size; a++)
+		if (cmp(array[a]))
 		{
-			if (cmp(array[a]))
-			{
-				return (a);
-			}
+			return (a);
 		}
-	}
-	if (size <= 0 || cmp == NULL)
-	{
-		return (-1);
 	}
 	return (-1);
 }
