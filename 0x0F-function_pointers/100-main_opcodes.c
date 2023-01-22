f74dcb82216d388f687650b4e4e4802fcb4b48d1
OmobaVII <omoba.career@gmail.com>
2023-01-22 06:15:26 -0800

    Advanced Task

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -10,11 +10,32 @@
 
 int main(int argc, char *argv[])
 {
-	int a = 0;
+	int b;
+	int a;
+	char *opcode;
 
-	while (a < atoi(argv[1]))
+	opcode = (char *)main;
+
+	if (argc != 2)
 	{
-		printf("%x\n", a);
-		a++;
+		printf("Error\n");
+		exit(1);
 	}
+	b = atoi(argv[1]);
+
+	if (b < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	for (a = 0; a < b; a++)
+	{
+		if (a == (b - 1))
+		{
+			printf("%02hhx\n", opcode[a]);
+		}
+		printf("%02hhx ", opcode[a]);
+	}
+	return (0);
 }
