708b1ba35ddccd8f52702744e464a7d673365b3a
OmobaVII <omoba.career@gmail.com>
2023-01-26 12:55:57 -0800

    1 Advanced

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * execute_first - function that prints before the main function
+ * Return:Void
+ */
+void execute_first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
