9944c6f62161f393c242af0b40e9cf1c266aac3d
OmobaVII <omoba.career@gmail.com>
2023-01-26 07:40:02 -0800

    Task 1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * list_len - a function that returns the number of element in a singly linked list
+ * @h: the singly linked list
+ * Return: the number of elements in the singly linked list
+ */
+
+size_t list_len(const list_t *h)
+{
+	int count = 0;
+
+	while (h != 0)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
