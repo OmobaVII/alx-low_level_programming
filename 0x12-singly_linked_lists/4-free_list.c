23000665b03fd77dc7e15d9409efb52a4d362390
OmobaVII <omoba.career@gmail.com>
2023-01-26 12:16:45 -0800

    Task 4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_list - a function that frees a singly linked list
+ * @head - the singly linked list
+ * Return: void
+ */
+
+void free_list(list_t *head)
+{
+
+	if (head == NULL)
+		return;
+	while (head != NULL)
+	{
+		free(head->str);
+		head->len = 0;
+		free(head);
+		head = head->next;
+	}
+}
