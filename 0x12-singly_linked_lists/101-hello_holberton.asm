a43e1bdce081bcb537f82eb665f1a2bade9db86c
OmobaVII <omoba.career@gmail.com>
2023-01-27 01:46:31 -0800

    2 Advanced Task

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,17 @@
+extern printf
+
+section .data
+    hello db 'Hello Holberton',10,0
+
+section .text
+    global main
+
+main:
+    ; printf("Hello Holberton\n")
+    mov rdi, hello
+    mov eax, 0
+    call printf
+    ; exit(0)
+    mov eax, 85
+    xor edi, edi
+    syscall
