b73de2698caeb1619b3b8aa30a6ef07f6d544dbc
OmobaVII <omoba.career@gmail.com>
2023-01-30 10:10:37 -0800

    Correction Task 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,16 +8,14 @@
 
 void free_listint2(listint_t **head)
 {
-	listint_t *ptr;
+	listint_t *ptr, *next;
 
-	if (*head == NULL)
-		return;
 	ptr = *head;
 	while (ptr != NULL)
 	{
+		next = ptr->next;
 		free(ptr);
-		ptr = ptr->next;
+		ptr = next;
 	}
-	free (ptr);
-	ptr = NULL;
+	*head = NULL;
 }
