ce983561e49996032e7e225de3dfb1a453014058
OmobaVII <omoba.career@gmail.com>
2023-01-30 03:50:04 -0800

    Task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - a function that frees a singly linked list
+ * and sets the head to NULL
+ * Return: void
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *ptr;
+
+	if (*head == NULL)
+		return;
+	ptr = *head;
+	while (ptr != NULL)
+	{
+		free(ptr);
+		ptr = ptr->next;
+	}
+	free(ptr);
+	ptr = NULL;
+}
