4936a7ea952e8ad93f554616a37943dcebedcbc3
OmobaVII <omoba.career@gmail.com>
2023-01-30 11:50:05 -0800

    Betty Correction

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -33,6 +33,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		{
 			new->next = *head;
 			*head = new;
+			return (new);
 		}
 		ptr = *head;
 		for (a = 0; a < (idx - 1) && ptr->next != NULL; a++)
