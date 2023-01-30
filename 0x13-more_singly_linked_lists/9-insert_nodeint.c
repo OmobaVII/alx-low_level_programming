c73f9aae0698ee9b3d1668bfbc7ca53a8503abac
OmobaVII <omoba.career@gmail.com>
2023-01-30 11:38:04 -0800

    Correction

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -23,13 +23,18 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 	else
 	{
-		ptr = *head;
 		new = malloc(sizeof(listint_t));
 		if (new == NULL)
 			return (NULL);
 		new->n = n;
 		new->next = NULL;
 
+		if (*head == NULL || idx == 0)
+		{
+			new->next = *head;
+			*head = new;
+		}
+		ptr = *head;
 		while (idx != 1)
 		{
 			ptr = ptr->next;
