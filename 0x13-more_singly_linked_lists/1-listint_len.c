81c8856096ec2de38845435b0ebf6953349756f7
OmobaVII <omoba.career@gmail.com>
2023-01-30 01:18:18 -0800

    Task 2

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * listint_len - a function that returns the number of elements
+ * in a linked list
+ * @h: the singly linked list
+ * Return: the number of elements
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	int nodes = 0;
+
+	while (h != NULL)
+	{
+		nodes++;
+		h = h->next;
+	}
+	return (nodes);
+}
