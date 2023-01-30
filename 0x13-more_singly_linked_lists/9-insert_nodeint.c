8a3462c46e72c7abaf1ceac8214cec5f49099905
OmobaVII <omoba.career@gmail.com>
2023-01-30 10:57:50 -0800

    Correction

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -19,7 +19,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new->n = n;
 	new->next = NULL;
 
-	while (idx != 2)
+	while (idx != 1)
 	{
 		ptr = ptr->next;
 		idx--;
