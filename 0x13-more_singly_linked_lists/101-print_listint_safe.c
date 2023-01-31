dd17a8914219ced8caa5b6220f654270ee0c7759
OmobaVII <omoba.career@gmail.com>
2023-01-31 09:36:16 -0800

    Correction

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -48,7 +48,8 @@ int print_list(const listint_t *head, int *visited)
 size_t print_listint_safe(const listint_t *head)
 {
 	int visited[10001] = {0};
-
+	if (head == NULL)
+		exit(98);
 	return (print_list(head, visited));
 }
 
