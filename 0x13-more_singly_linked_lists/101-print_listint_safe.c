3dc6d779e839b4cf290c137529ff99e7784378ef
OmobaVII <omoba.career@gmail.com>
2023-01-31 09:30:41 -0800

    Initial commit

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,50 +1,54 @@
 #include "lists.h"
 #include <stdio.h>
-
+#include <stdlib.h>
 /**
- * print_listint_safe - a function that prints a list
- * @head: the singly linked list
- * Return: the number of nodes
+ * mark_node - a function that marks the nodes visited
+ * @node: the node visited
+ * @visited: a pointer to the visited node
+ * Return: 0 or 1.
  */
-
-size_t print_listint_safe(const listint_t *head)
+int mark_node(listint_t *node, int *visited)
 {
-	const listint_t *current, *temp;
+	if (visited[node->n] == 1)
+	{
+		return (1);
+	}
+	visited[node->n] = 1;
+	return (0);
+}
+/**
+ * print_list - a function used to print a list
+ * @head: the list to print
+ * @visited: a flag to mark a list that has been printed
+ * Return: void
+ */
+int print_list(const listint_t *head, int *visited)
+{
+	const listint_t *current = head;
 	int nodes = 0;
-	int a = 0;
 
-	current = head;
-	temp = head;
 	while (current != NULL)
 	{
-		while (temp != current)
-		{
-			if (temp->next == current)
-			{
-				a = 1;
-				break;
-			}
-			temp = temp->next;
-		}
-	if (a == 1)
-	{
-		if (temp == current)
+		if (mark_node((listint_t *)current, visited) == 1)
 		{
 			printf("[%p] %d\n", (void *)current, current->n);
 			break;
 		}
-	}
-	else
-	{
-		nodes++;
 		printf("[%p] %d\n", (void *)current, current->n);
+		nodes++;
 		current = current->next;
-	}
-
-	if (head == NULL)
-	{
-		exit(98);
-	}
 	}
 	return (nodes);
 }
+/**
+ * print_listint_safe - a function that prints a list including those in a loop
+ * @head: the singly linked list to print
+ * Return: the total number of nodes
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	int visited[10001] = {0};
+
+	return (print_list(head, visited));
+}
+
