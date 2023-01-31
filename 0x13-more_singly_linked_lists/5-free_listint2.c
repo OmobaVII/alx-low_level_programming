74d7f6295de4654f76b59ddb6407fbe8c1248395
OmobaVII <omoba.career@gmail.com>
2023-01-31 11:11:08 -0800

    Correction

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -11,7 +11,7 @@ void free_listint2(listint_t **head)
 {
 	listint_t *ptr;
 
-	if (*head == NULL)
+	if (head == NULL)
 	{
 		return;
 	}
@@ -21,4 +21,5 @@ void free_listint2(listint_t **head)
 		*head = ptr->next;
 		free(ptr);
 	}
+	*head = NULL;
 }
