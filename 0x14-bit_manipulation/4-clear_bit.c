d30ffa8c5bbde7aa6c5bbd60aa96a1667a4ec469
OmobaVII <omoba.career@gmail.com>
2023-02-02 06:52:51 -0800

    Betty Correction

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -3,7 +3,7 @@
 /**
  * clear_bit - a function that sets the value of a bit at an index to 0.
  * @n: the number to be set
- * @index the location to be set
+ * @index: the location to be set
  * Return: 1 if success and -1 if failed
  */
 int clear_bit(unsigned long int *n, unsigned int index)
