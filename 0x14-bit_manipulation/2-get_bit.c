92e6c22149e4e36b38e5c93adbc8341d78b11cae
OmobaVII <omoba.career@gmail.com>
2023-02-02 04:39:26 -0800

    Betty Correction

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * get_bit a function that returns the value of a bit at a given index
+ * get_bit - a function that returns the value of a bit at a given index
  * @n: the number
  * @index: the point to check the bit there
  * Return: the value of the bit at the index
@@ -9,7 +9,7 @@
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-	if (!(n))
+	if (index >= sizeof(int) * 8)
 		return (-1);
 	return (n & (1 << index)) != 0;
 }
