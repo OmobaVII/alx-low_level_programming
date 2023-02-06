860184f968de5809e5abc4cc87abd70ccacc661c
OmobaVII <omoba.career@gmail.com>
2023-02-06 11:26:45 -0800

    Correction Betty

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -12,22 +12,18 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	FILE *fp;
 	char buffer[1024];
-	unsigned long int read_characters;
+	unsigned long int read_characters = 0;
 	int line_len, real;
 
-	read_characters = 0;
 	real = letters - read_characters;
-
 	if (filename == NULL)
-	{
 		return (0);
-	}
 	fp = fopen(filename, "r");
 	if (fp == NULL)
-	{
 		return (0);
-	}
-	while (fgets(buffer, 1024, fp) != NULL && read_characters < letters)
+	if (letters == 0)
+		return (0);
+	while (fgets(buffer, 1024, fp) != NULL && (read_characters < letters))
 	{
 		line_len = strlen(buffer);
 		if (line_len <= real)
@@ -41,7 +37,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		}
 		else
 		{
-			if (printf("%.*s\n", (real), buffer) < (real))
+			if (printf("%.*s\n", (int)(real), buffer) < (real))
 			{
 				fclose(fp);
 				return (0);
