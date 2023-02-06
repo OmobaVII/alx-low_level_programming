0a046eeaa49f9cadd38ecd8e71a12355f294bf51
OmobaVII <omoba.career@gmail.com>
2023-02-06 06:05:02 -0800

    Under the snow

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * create_file - a function that creates a file
+ * @filename: the name of the file to create
+ * @text_content: file content
+ * Return: Success (1), Fail (-1)
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fd == -1)
+	{
+		return (-1);
+	}
+	if (text_content != NULL)
+	{
+		if (write(fd, text_content, strlen(text_content)) == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+	close (fd);
+	return (1);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -3,8 +3,12 @@
 #include <stddef.h>
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+
 
 
 #endif
