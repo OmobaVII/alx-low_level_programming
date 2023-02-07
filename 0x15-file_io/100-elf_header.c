b7f60d08ee3a5d8d06ef8589c4dc13a6d20d978a
OmobaVII <omoba.career@gmail.com>
2023-02-07 04:24:57 -0800

    Correction

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -33,16 +33,14 @@ void display_elf_header(Elf64_Ehdr *ehdr)
 		printf("%02x ", ehdr->e_ident[i]);
 	}
 	printf("\n");
-	printf("  Class:                             ELF%d\n", ehdr->e_ident[4]);
+	printf("  Class:                             ELF%d\n", ehdr->e_ident[4] == ELFCLASS32 ? 32 : 64);
 	printf("  Data:                              2's complement, %s endian\n", (ehdr->e_ident[5] == ELFDATA2LSB) ? "little" : "big");
-	printf("  Version:                           %d\n", ehdr->e_ident[6]);
+	printf("  Version:                           %d (current)\n", ehdr->e_ident[6]);
 	printf("  OS/ABI:                            UNIX - System V\n");
 	printf("  ABI Version:                       %d\n", ehdr->e_ident[8]);
 	printf("  Type:                              %s\n", (ehdr->e_type == ET_EXEC) ? "EXEC (Executable file)" :
 			(ehdr->e_type == ET_DYN) ? "DYN (Shared object file)" :
 			(ehdr->e_type == ET_REL) ? "REL (Relocatable file)" : "NONE");
-	printf("  Machine:                           %s\n", (ehdr->e_machine == EM_X86_64) ? "Advanced Micro Devices X86-64" : "UNKNOWN");
-	printf("  Version:                           0x%x\n", ehdr->e_version);
 	printf("  Entry point address:               0x%0lx\n", ehdr->e_entry);
 }
 
