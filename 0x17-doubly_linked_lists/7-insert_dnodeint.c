49d6355afd34ad10d98ff59ed103d81d6fa6c8d9
OmobaVII <omoba.career@gmail.com>
2023-03-16 07:24:05 -0700

    Insert node at index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -11,27 +11,18 @@
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new, *current, *temp, *temp2;
-	unsigned int pos, count;
-
-	current = *h;
-	temp = *h;
-	pos = 0;
-	count = 0;
+	dlistint_t *current = *h, *temp = *h, *new, *temp2;
+	unsigned int pos = 0, count = 0;
 
 	if (*h == NULL)
-	{
 		return (NULL);
-	}
 	while (temp != NULL)
 	{
 		count++;
 		temp = temp->next;
 	}
-	if ((idx + 1) > count)
-	{
+	if (idx + 1 > count)
 		return (NULL);
-	}
 	while (pos != idx)
 	{
 		pos++;
@@ -39,16 +30,25 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	}
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
-	{
 		return (NULL);
-	}
-
 	new->n = n;
+	if (idx == 0)
+	{
+		new->prev = NULL;
+		new->next = *h;
+		(*h)->prev = new;
+		return (new);
+	}
+	if (current->next == NULL)
+	{
+		current->next = new;
+		new->prev = current;
+		return (new);
+	}
 	temp2 = current->prev;
 	current->prev = new;
 	temp2->next = new;
 	new->next = current;
 	new->prev = temp2;
-
 	return (new);
 }
