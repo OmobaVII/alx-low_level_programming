5aba572ee7e0f7a58bb608e83ec13d049e00633d
OmobaVII <omoba.career@gmail.com>
2023-03-16 07:33:57 -0700

    Insert node at index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -14,7 +14,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *current = *h, *temp = *h, *new, *temp2;
 	unsigned int pos = 0, count = 0;
 
-	if (*h == NULL)
+	if (h == NULL)
 		return (NULL);
 	while (temp != NULL)
 	{
